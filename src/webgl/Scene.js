992e889b3f6bb2109dd70bd660b475711cf6f6d4
Manu TEAO <mrgeymeurt@gmail.com>
2024-12-20 15:35:12 +0100

    Update Scene.js

--- src/webgl/Scene.js
+++ src/webgl/Scene.js
@@ -9,9 +9,43 @@ class Scene {
         this.canvas = canvas;
         this.width = window.innerWidth;
         this.height = window.innerHeight;
-        
-        // Instantier la logique three.js
+
+        this.setupScene();
+        this.setupCamera();
+        this.setupRenderer();
+
+        this.addEvent();
+        this.addObjects();
+    }
+
+    addObjects() {
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        this.material = new THREE.MeshNormalMaterial();
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+        this.scene.add(this.mesh);
+    }
+
+    onResize() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+
+        this.camera.aspect = this.width / this.height;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(this.width, this.height);
+    }
+
+    addEvent() {
+        gsap.ticker.add(this.tick);
+        window.addEventListener("resize", this.onResize);
+    }
+
+    setupScene() {
         this.scene = new THREE.Scene();
+    }
+
+    setupCamera() {
         this.camera = new THREE.PerspectiveCamera(
             75,
             this.width / this.height,
@@ -20,20 +54,16 @@ class Scene {
         );
 
         this.camera.position.z = 5;
+    }
 
+    setupRenderer() {
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.canvas,
             antialias: false,
         });
 
         this.renderer.setSize(this.width, this.height);
-
-        this.geometry = new THREE.BoxGeometry(1, 1, 1);
-        this.material = new THREE.MeshNormalMaterial();
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-
-        this.scene.add(this.mesh);
-        gsap.ticker.add(this.tick);
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     }
 
     tick = (time, deltaTime, frame) => {
